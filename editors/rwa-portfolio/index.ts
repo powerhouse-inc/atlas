2eff48a1d0d4b07f98642d0111f951808c935986
ryanwolhuter <dev@ryanwolhuter.com>
2025-01-22 14:30:00 +0200

    feat: copy over rwa portfolio from powerhouse monorepo
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

---
+++ editors/rwa-portfolio/index.ts
@@ -0,0 +1,24 @@
+import { RWAEditorContextProps } from "@powerhousedao/design-system";
+import type {
+    RealWorldAssetsState,
+    RealWorldAssetsLocalState,
+    RealWorldAssetsAction,
+} from "../../document-models/rwa-portfolio";
+import type { ExtendedEditor } from "../types";
+import { lazyWithPreload } from "document-model-libs/utils";
+
+export const module: ExtendedEditor<
+  RealWorldAssetsState,
+  RealWorldAssetsAction,
+  RealWorldAssetsLocalState,
+  RWAEditorContextProps
+> = {
+  Component: lazyWithPreload(() => import("./editor")),
+  documentTypes: ["makerdao/rwa-portfolio"],
+  config: {
+    id: "rwa-editor",
+    disableExternalControls: true,
+  },
+};
+
+export default module;
