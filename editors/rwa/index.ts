5c785045ba33922f3d1bc7194a308618bde0fcc5
ryanwolhuter <dev@ryanwolhuter.com>
2025-01-23 16:10:24 +0200

    feat: remove lazy preload
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- editors/rwa/index.ts
+++ editors/rwa/index.ts
@@ -4,16 +4,15 @@ import type {
   RealWorldAssetsLocalState,
   RealWorldAssetsAction,
 } from "../../document-models/real-world-assets";
-import type { ExtendedEditor } from "../types";
-import { lazyWithPreload } from "document-model-libs/utils";
-
+import { ExtendedEditor } from "document-model-libs";
+import Component from "./editor";
 export const module: ExtendedEditor<
   RealWorldAssetsState,
   RealWorldAssetsAction,
   RealWorldAssetsLocalState,
   RWAEditorContextProps
 > = {
-  Component: lazyWithPreload(() => import("./editor")),
+  Component,
   documentTypes: ["makerdao/rwa-portfolio"],
   config: {
     id: "rwa-editor",
