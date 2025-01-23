2f54ea45af9fe9048aced65bc96a440744010a28
ryanwolhuter <dev@ryanwolhuter.com>
2025-01-23 09:47:36 +0200

    feat: make libs a dep
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- editors/rwa-portfolio/index.ts
+++ editors/rwa-portfolio/index.ts
@@ -4,16 +4,15 @@ import type {
   RealWorldAssetsLocalState,
   RealWorldAssetsAction,
 } from "../../document-models/rwa-portfolio";
-import { lazyWithPreload } from "document-model-libs/utils";
 import { ExtendedEditor } from "document-model-libs";
-
+import Editor from "./editor";
 export const module: ExtendedEditor<
   RealWorldAssetsState,
   RealWorldAssetsAction,
   RealWorldAssetsLocalState,
   RWAEditorContextProps
 > = {
-  Component: lazyWithPreload(() => import("./editor")),
+  Component: Editor,
   documentTypes: ["makerdao/rwa-portfolio"],
   config: {
     id: "rwa-editor",
