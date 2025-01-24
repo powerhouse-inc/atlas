3ad419dc84868ecda6ee4abf61ebc91900ba669a
ryanwolhuter <dev@ryanwolhuter.com>
2025-01-24 14:26:26 +0200

    feat: temp remove invoice and accounts
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- document-models/index.ts
+++ document-models/index.ts
@@ -1,7 +1,5 @@
 export { module as AtlasScope } from "./atlas-scope";
 export { module as AtlasFoundation } from "./atlas-foundation";
-export { module as Accounts } from "./accounts";
 export { module as AccountTransactions } from "./account-transactions";
 export { module as AtlasFeedbackIssues } from "./atlas-feedback-issues";
 export { module as RealWorldAssets } from "./real-world-assets";
-export { module as Invoice } from "./invoice";
