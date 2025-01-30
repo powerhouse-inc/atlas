52ff5d84360b8b9fa293a0dd456f80804b2098ba
liberuum <liberom@proton.me>
2025-01-30 10:49:39 +0000

    update cors policy

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,7 @@ import cors from 'cors';
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000', // Allow requests from your React app
+    origin: '*', // Allow requests from your React app
     credentials: true
   }));
 
