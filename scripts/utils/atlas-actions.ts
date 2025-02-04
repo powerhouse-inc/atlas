d0e5112fdd4850d6308d276acce7bc52880798c0
liberuum <liberom@proton.me>
2025-02-04 13:07:43 +0000

    update deps + scripts

--- scripts/utils/atlas-actions.ts
+++ scripts/utils/atlas-actions.ts
@@ -2,6 +2,8 @@ import { IBaseDocumentDriveServer } from "document-drive/server";
 import { actions as atlasScopeActions } from "../../document-models/atlas-scope";
 import { actions as atlasFoundationActions } from "../../document-models/atlas-foundation";
 import { addFolder, addDocument } from "./drive-actions";
+import notionSections from "../atlas-scope/data/notion-pages/section.json";
+import viewNodeTree from "../atlas-scope/data/view-node-tree.json";
 
 export const populateScope = (
   driveServer: IBaseDocumentDriveServer,
@@ -9,7 +11,7 @@ export const populateScope = (
   documentId: string,
   scope: any,
   provenanceUrl: string | undefined,
-  jsonMasterStatus: any,
+  jsonMasterStatus: any
 ) => {
   return driveServer.addAction(
     driveId,
@@ -32,7 +34,7 @@ export const populateScope = (
       originalContextData: ["somePHID"],
       provenance: provenanceUrl,
       notionId: scope.id,
-    }),
+    })
   );
 };
 
@@ -41,17 +43,17 @@ export const populateArticle = (
   driveId: string,
   documentId: string,
   article: any,
-  scope: any,
+  scope: any
 ) => {
   return driveServer.addAction(
     driveId,
     documentId,
     atlasFoundationActions.populateFoundation({
-      name: article.properties.Name.rich_text[0].plain_text,
+      name: article.properties["Name"].rich_text[0].plain_text,
       docNo: article.properties["Doc No"].title[0].plain_text,
       parent: scope.id,
       atlasType: "ARTICLE",
-      content: article.properties.Content.rich_text[0].plain_text,
+      content: article.properties["Content"].rich_text[0].plain_text,
       masterStatus: "PLACEHOLDER",
       globalTags: ["CAIS"],
       references: [],
@@ -60,9 +62,40 @@ export const populateArticle = (
       ].relation.map((r: any) => r.id),
       provenance: article.url,
       notionId: article.id,
-    }),
+    })
   );
 };
+
+export const populateSection = (
+  driveServer: IBaseDocumentDriveServer,
+  driveId: string,
+  documentId: string,
+  section: any,
+  article: any
+) => {
+  try {
+    return driveServer.addAction(
+      driveId,
+      documentId,
+      atlasFoundationActions.populateFoundation({
+        name: section.properties["Name"].formula.string,
+        docNo: section.properties["Doc No (or Temp Name)"].title[0].plain_text,
+        parent: article.id,
+        atlasType: "SECTION",
+        content: section.properties["Content"].rich_text?.[0]?.plain_text || '',
+        masterStatus: "PLACEHOLDER",
+        globalTags: ["CAIS"],
+        references: [],
+        originalContextData: [],
+        provenance: section.url,
+        notionId: section.id,
+      })
+    );
+  } catch (error) {
+    console.error("Error populating section:", section.id, section.properties["Name"].formula.string, error);
+  }
+};
+
 
 export const addArticles = async (
   driveServer: IBaseDocumentDriveServer,
@@ -70,7 +103,7 @@ export const addArticles = async (
   documentId: string,
   articlesIds: any[],
   notionArticles: any[],
-  scope: any,
+  scope: any
 ) => {
   for (const articleObj of articlesIds) {
     const article = notionArticles.find((a: any) => a.id === articleObj.id);
@@ -82,16 +115,16 @@ export const addArticles = async (
       driveServer,
       driveId,
       article.id + "-folder",
-      `${article.properties["Doc No"].title[0].plain_text} ${article.properties.Name.rich_text[0].plain_text}`,
-      scope.id + "-folder",
+      `${article.properties["Doc No"].title[0].plain_text} ${article.properties["Name"].rich_text[0].plain_text}`,
+      scope.id + "-folder"
     );
     await addDocument(
       driveServer,
       driveId,
       article.id + "-document",
-      `${article.properties["Doc No"].title[0].plain_text} ${article.properties.Name.rich_text[0].plain_text}`,
+      `${article.properties["Doc No"].title[0].plain_text} ${article.properties["Name"].rich_text[0].plain_text}`,
       "sky/atlas-foundation",
-      article.id + "-folder",
+      article.id + "-folder"
     );
 
     const result = await populateArticle(
@@ -99,7 +132,80 @@ export const addArticles = async (
       driveId,
       article.id + "-document",
       article,
-      scope,
+      scope
+    );
+
+
+    const subDocuments = getSubDocuments(article.id);
+    // add sections to article
+    await addSections(
+      driveServer,
+      driveId,
+      article.id + "-folder",
+      subDocuments,
+      notionSections,
+      article
     );
   }
 };
+
+export const addSections = async (
+  driveServer: IBaseDocumentDriveServer,
+  driveId: string,
+  parentFolderId: string,
+  sectionIds: any[],
+  notionSections: any,
+  article: any
+) => {
+
+  for (const sectionId of sectionIds) {
+    const section = notionSections.find((s: any) => s.id === sectionId);
+    if (!section) {
+      continue;
+    }
+
+
+    await addFolder(
+      driveServer,
+      driveId,
+      section.id + "-folder",
+      section.properties["Doc No (or Temp Name)"].title[0].plain_text,
+      parentFolderId
+    );
+
+    await addDocument(
+      driveServer,
+      driveId,
+      section.id + "-document",
+      section.properties["Doc No (or Temp Name)"].title[0].plain_text,
+      "sky/atlas-foundation",
+      section.id + "-folder"
+    );
+
+    await populateSection(
+      driveServer,
+      driveId,
+      section.id + "-document",
+      section,
+      article
+    );
+
+    await sleep(500);
+  }
+};
+
+const getSubDocuments = (id: string) => {
+  const sd = Object.values(viewNodeTree).find((node) => node.id === id);
+  const subDocuments = sd.subDocuments.map((sd) => sd.id);
+  // const subDocuments = sd.subDocuments.map((sd) => ({
+  //   id: sd.id,
+  //   title: sd.title.title,
+  //   formalId: sd.title.formalId.prefix + sd.title.formalId.numberPath.join("."),
+  // }));
+
+  // console.log('subDocuments', subDocuments);
+
+  return subDocuments;
+}
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
